f1049cc7137acfb4d406b70dfb5ce33544ab45dd
PaniMartyna <martyna.machowska@outlook.com>
2022-12-08 18:00:58 +0100

    detailed meal view only get

--- plans/urls.py
+++ plans/urls.py
@@ -6,6 +6,6 @@ app_name = 'plans'
 
 urlpatterns = [
     path('week_plan/', views.WeekPlanView.as_view(), name='week-plan'),
-    # path('cookbook/', views.RecipeListView.as_view(), name='recipe-list'),
+    path('plan/<str:day>/<int:meal_id>', views.PlanDetailView.as_view(), name='plan-detail'),
     # path('show_recipe/<int:idx>/', views.RecipeShowView.as_view(), name='show-recipe'),
 ]
--- plans/views.py
+++ plans/views.py
@@ -1,7 +1,11 @@
-from datetime import date, timedelta
+from datetime import date, timedelta, datetime
 
 from django import views
+from django.http import HttpResponse
 from django.shortcuts import render
+
+from preferences.models import MealSetting
+from recipes.models import Recipe
 
 
 class WeekPlanView(views.View):
@@ -18,3 +22,18 @@ class WeekPlanView(views.View):
 
         })
 
+
+class PlanDetailView(views.View):
+
+    def get(self, request, day, meal_id):
+        meal = MealSetting.objects.get(id=meal_id)
+        meal_date = datetime.strptime(day, '%Y-%m-%d').date()
+        recipe_list = Recipe.objects.all()
+        return render(request, 'plans/plan_detail.html', {
+            'meal_date': meal_date,
+            'meal': meal,
+            'recipe_list': recipe_list,
+        })
+
+    def post(self, request):
+        pass
