501fe179d083a371179d8276cd81391a288dcfb8
PaniMartyna <martyna.machowska@outlook.com>
2022-12-08 23:31:56 +0100

    week plan redirects stage 1

--- plans/urls.py
+++ plans/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = 'plans'
 
 urlpatterns = [
-    path('week_plan/', views.WeekPlanView.as_view(), name='week-plan'),
-    path('plan/<str:day>/<int:meal_id>', views.PlanDetailView.as_view(), name='plan-detail'),
+    path('week_plan/<str:week_start>/', views.WeekPlanView.as_view(), name='week-plan'),
+    path('week_plan/<str:week_start>/plan/<str:day>/<int:meal_id>', views.PlanDetailView.as_view(), name='plan-detail'),
     # path('show_recipe/<int:idx>/', views.RecipeShowView.as_view(), name='show-recipe'),
 ]
--- plans/views.py
+++ plans/views.py
@@ -2,7 +2,8 @@ from datetime import date, timedelta, datetime
 
 from django import views
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
 
 from preferences.models import MealSetting
 from recipes.models import Recipe
@@ -10,13 +11,15 @@ from recipes.models import Recipe
 
 class WeekPlanView(views.View):
 
-    def get(self, request):
+    def get(self, request, week_start):
         user = request.user
         """show 7 days of upcoming week"""
-        base = date.today() + timedelta(days=7-date.today().weekday())
+        # base = date.today() + timedelta(days=7-date.today().weekday())
+        base = datetime.strptime(week_start, '%Y-%m-%d').date()
         day_list = [base + timedelta(days=x) for x in range(7)]
         meal_list = user.selected_meals.all()
         return render(request, 'plans/week_plan.html', {
+            'week_start': week_start,
             'day_list': day_list,
             'meal_list': meal_list,
 
@@ -25,7 +28,7 @@ class WeekPlanView(views.View):
 
 class PlanDetailView(views.View):
 
-    def get(self, request, day, meal_id):
+    def get(self, request, week_start, day, meal_id):
         meal = MealSetting.objects.get(id=meal_id)
         meal_date = datetime.strptime(day, '%Y-%m-%d').date()
         recipe_list = Recipe.objects.all()
@@ -35,5 +38,8 @@ class PlanDetailView(views.View):
             'recipe_list': recipe_list,
         })
 
-    def post(self, request):
-        pass
+    def post(self, request, week_start):
+        selected_recipes = request.POST.getlist['recipes']
+        print(selected_recipes)
+
+        return redirect(reverse_lazy('plans:week-plan', week_start=week_start))
